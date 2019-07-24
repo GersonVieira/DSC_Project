bd1471a4ec15909d5dad250cd2bda48007c247ed
Douglas Cardins <douglas.cardins@gmail.com>
2019-07-24 13:56:08 -0300

    Tela cadastro adicionada. Barra de pesquisa adicionada

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,17 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { FilmesComponent } from './filmes/filmes.component';
 import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-
+/*
+* O path diz qual vai ser o caminho e logo depois
+* coloca o componente que esse caminho vai buscar
+*/
 const APP_ROUTE: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '', component: FilmesComponent }
+  { path: '', component: FilmesComponent },
+  { path: 'cadastro', component: CadastroComponent }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTE);
--- src/app/contribuidores/contribuidores.component.ts
+++ src/app/contribuidores/contribuidores.component.ts
@@ -7,15 +7,15 @@ import { Contribuidor } from '../contribuidor/contribuidor.model';
   styleUrls: ['./contribuidores.component.css']
 })
 export class ContribuidoresComponent implements OnInit {
-  contribuidores:Contribuidor[];
-  constructor() { 
+  contribuidores: Contribuidor[];
+  constructor() {
     this.contribuidores = [
       new Contribuidor('Gefferson Pires', 'Graduando em LCC', "https://github.com/geffpires", "/assets/images/gefffoto.jpg", 'GitHub'),
       new Contribuidor('Gerson Pires', 'Graduando em LCC', "https://github.com/gersonvieira", "/assets/images/gersonft.jpg", 'GitHub'),
       new Contribuidor('Douglas Cardins', 'Graduando em SI', "https://github.com/DouglasTex", "https://avatars1.githubusercontent.com/u/20545738?s=460&v=4", 'GitHub')
-    ];  
+    ];
   }
-  
+
   ngOnInit() {
   }
 
