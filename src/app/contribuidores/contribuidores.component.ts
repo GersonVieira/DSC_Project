170bea777dfddb4a6050572a8b3ca5cab0f3dae1
Francisco <francisco.chagas@dce.ufpb.br>
2019-07-26 13:40:18 -0300

    novo contribuidor

--- src/app/contribuidores/contribuidores.component.ts
+++ src/app/contribuidores/contribuidores.component.ts
@@ -12,7 +12,8 @@ export class ContribuidoresComponent implements OnInit {
     this.contribuidores = [
       new Contribuidor('Gefferson Pires', 'Graduando em LCC', "https://github.com/geffpires", "/assets/images/gefffoto.jpg", 'GitHub'),
       new Contribuidor('Gerson Pires', 'Graduando em LCC', "https://github.com/gersonvieira", "/assets/images/gersonft.jpg", 'GitHub'),
-      new Contribuidor('Douglas Cardins', 'Graduando em SI', "https://github.com/DouglasTex", "https://avatars1.githubusercontent.com/u/20545738?s=460&v=4", 'GitHub')
+      new Contribuidor('Douglas Cardins', 'Graduando em SI', "https://github.com/DouglasTex", "https://avatars1.githubusercontent.com/u/20545738?s=460&v=4", 'GitHub'),
+      new Contribuidor('Francisco das Chagas', 'Graduando em SI', "https://github.com/franciscochagas13", "https://avatars1.githubusercontent.com/u/38105241?s=400&u=a98ce3dc642501c5a581a68464357b41aa47edfc&v=4", 'GitHub')
     ];  
   }
   
